8da063ec0ccae2c79b10e90fd676b95bd5dcd4b2
Matinrzv <razavimatin36@gmail.com>
2025-10-12 22:17:27 +0330

    توضیح تغییرات

--- Projects/Download_Youtube.py
+++ Projects/Download_Youtube.py
@@ -1,6 +1,10 @@
 import yt_dlp
-url = input("Enter Your Video URL: ")
+
+url = input("Enter video url: ")
+
 ydl_opts = {}
+
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
-print("Downloaded")
+    
+print("Video downloaded successfully")
